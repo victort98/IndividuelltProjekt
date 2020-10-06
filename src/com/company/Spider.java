92810ee33e053dae917a56f9579f6c201a84dbaa
Victor <victortornbom@gmail.com>
2020-10-06 10:33:26 +0200

    Added spider monster

---
+++ src/com/company/Spider.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Random;
+
+public class Spider extends Monsters{
+
+    public Spider(int health, int damage, String damageInfo, String name) {
+        super(health, damage, damageInfo, name);
+    }
+
+    private Random rand = new Random();
+
+    @Override
+    public void information() {
+        System.out.println("The spider has " + health + " health and can hit for 3-5 damage");
+    }
+
+    public int getSpiderDamage() {
+        return damage = rand.nextInt(5 - 3 + 1) + 3;
+    }
+}
