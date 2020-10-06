53a1cba9da2c9082c5ab3495dbc0fe7b034f762e
Victor <victortornbom@gmail.com>
2020-10-06 10:34:06 +0200

    Added super class Creature

---
+++ src/com/company/Creature.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public abstract class Creature {
+    int health;
+    int damage;
+    String damageInfo;
+    String name;
+
+    public Creature(int health, int damage, String damageInfo, String name) {
+        this.health = health;
+        this.damage = damage;
+        this.damageInfo = damageInfo;
+        this.name = name;
+    }
+
+    public abstract void information();
+}
