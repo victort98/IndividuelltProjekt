a3cd1c3644ade5c93f9d728f324a1a3f4e75cd4b
Victor <victortornbom@gmail.com>
2020-10-06 10:33:31 +0200

    Added Rat monster

---
+++ src/com/company/Rat.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Random;
+
+public class Rat extends Monsters{
+
+    public Rat(int health, int damage, String damageInfo, String name) {
+        super(health, damage, damageInfo, name);
+    }
+
+    private Random rand = new Random();
+
+    @Override
+    public void information() {
+        System.out.println("The rat has " + health + " health and can hit for 1-3 damage");
+    }
+
+    public int getRatDamage() {
+        return damage = rand.nextInt(3 - 1 + 1) + 1;
+    }
+}
